fdb349925878803f56fa36679d6e0f0cd3dc7424
Pistori1 <128402253+Pistori1@users.noreply.github.com>
2025-05-28 00:30:49 -0300

    Enviroments fix

--- src/app/environments.py
+++ src/app/environments.py
@@ -34,17 +34,25 @@ class Environments:
         
     @staticmethod
     def get_user_repo() -> IUserRepository:
+        if Environments._user_repo is not None:
+            return Environments._user_repo
+
         if Environments.get_envs().stage == STAGE.TEST:
             from .repo.user_repository_mock import UserRepositoryMock
-            return UserRepositoryMock()
+            Environments._user_repo = UserRepositoryMock()
+            return Environments._user_repo
         else:
             raise EnvironmentNotFound("STAGE")
-        
+
     @staticmethod
     def get_transaction_repo() -> ITransactionsRepository:
+        if Environments._transaction_repo is not None:
+            return Environments._transaction_repo
+
         if Environments.get_envs().stage == STAGE.TEST:
             from .repo.transaction_repository_mock import TransactionRepositoryMock
-            return TransactionRepositoryMock()
+            Environments._transaction_repo = TransactionRepositoryMock()
+            return Environments._transaction_repo
         else:
             raise EnvironmentNotFound("STAGE")
 
