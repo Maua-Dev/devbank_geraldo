d6d144b82c6e9bbd3bd58879e8c732cb4a66c1a4
Pistori1 <128402253+Pistori1@users.noreply.github.com>
2025-06-01 22:37:13 -0300

    err fix

--- src/app/main.py
+++ src/app/main.py
@@ -49,6 +49,7 @@ def deposit(request: dict = Body(...), user_repo=user_repo):
     current_time = time.time() * 1000
     
     user_repo.update_user(
+        user_id=1,
         name=user.name,
         agency=user.agency,
         account=user.account,
@@ -91,6 +92,7 @@ def withdrawal(request: dict = Body(...), user_repo=user_repo):
     current_balance = user.current_balance
 
     user_repo.update_user(
+        user_id=1,
         name=user.name,
         agency=user.agency,
         account=user.account,
