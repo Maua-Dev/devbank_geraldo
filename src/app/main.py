ec589f072dc53e62228c49d7e889ba372c73f237
Pistori1 <128402253+Pistori1@users.noreply.github.com>
2025-06-02 01:11:31 -0300

    user_repo in withdrawal

--- src/app/main.py
+++ src/app/main.py
@@ -67,6 +67,7 @@ def deposit(request: dict):
 
 @app.post("/withdrawal")
 def withdrawal(request: dict, user_repo):
+    user_repo = Environments.get_user_repo()
     user = user_repo.get_user(user_id=1)
 
     if not user:
