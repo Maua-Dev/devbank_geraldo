cc4d2eda71b49696339112f5736331e04b6d9654
Pistori1 <128402253+Pistori1@users.noreply.github.com>
2025-06-01 22:20:04 -0300

    update_balance

--- src/app/main.py
+++ src/app/main.py
@@ -48,6 +48,13 @@ def deposit(request: dict = Body(...), user_repo=user_repo):
 
     current_time = time.time() * 1000
 
+    user_repo.update_user(
+        name=user.name,
+        agency=user.agency,
+        account=user.account,
+        current_balance=current_balance
+    )
+
     transaction = transacation_repo.create_transaction(
         aux_type="deposit",
         value=float(total_deposit),
@@ -83,6 +90,13 @@ def withdrawal(request: dict = Body(...), user_repo=user_repo):
     
     user.current_balance = user.current_balance - total_withdrawal
     current_balance = user.current_balance
+
+    user_repo.update_user(
+        name=user.name,
+        agency=user.agency,
+        account=user.account,
+        current_balance=current_balance
+    )
 
     current_time = time.time() * 1000
 
